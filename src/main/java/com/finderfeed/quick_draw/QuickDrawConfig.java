07bf39488b23d5ae64e462eacdbccd318d2721b4
FINDERFEED <windingaster335@gmail.com>
2024-06-15 11:53:08 +0300

    1.0.0

--- src/main/java/com/finderfeed/quick_draw/QuickDrawConfig.java
+++ src/main/java/com/finderfeed/quick_draw/QuickDrawConfig.java
@@ -25,10 +25,10 @@ public class QuickDrawConfig {
         QUICK_DRAW_SLOT_COLOR = BUILDER.comment("Color in hex format that indicates which slot is a fast access slot")
                 .define("fast_access_slot_color","0x00ff00");
 
-        QUICK_DRAW_MENU_SELECTED = BUILDER.comment("Color in hex format (ARGB) when hovered over a section in quick draw menu")
+        QUICK_DRAW_MENU_SELECTED = BUILDER.comment("Color in hex format (RGB) when hovered over a section in quick draw menu")
                 .define("selected_section_color","0xaaaaaa");
 
-        QUICK_DRAW_MENU_UNSELECTED = BUILDER.comment("Color in hex format (ARGB) when of an unselected section in quick draw menu")
+        QUICK_DRAW_MENU_UNSELECTED = BUILDER.comment("Color in hex format (RGB) when of an unselected section in quick draw menu")
                 .define("unselected_section_color","0xeeeeee");
 
         RENDER_QUICK_DRAW_SLOT = BUILDER.comment("If true - always renders which slot is a fast access one, if false - renders only when ctrl+shift is pressed")
